63cc600829cbd65aea05870a97f31302a6545b30
Deshan Chinthana <chamildes@gmail.com>
2024-12-21 18:25:40 +0530

    react-icons installed, adminPanel design changed

--- src/pages/backend/AdminPanel.jsx
+++ src/pages/backend/AdminPanel.jsx
@@ -1,5 +1,47 @@
+import { BsBoxSeam, BsCart4, BsGraphUp, BsPeopleFill } from "react-icons/bs";
+import { Link, Route, Routes } from "react-router-dom";
+
 export default function AdminPanel() {
   return (
-    <div>AdminPanel</div>
+    <div className="bg-blue-200 w-full h-screen flex">
+      <div className="w-[20%] h-screen bg-blue-500 flex flex-col items-center py-4">
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-blue-200"
+          to="/admin/dashboard"
+        >
+          <BsGraphUp className="mr-2" /> Dashboard
+        </Link>
+
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-blue-200"
+          to="/admin/products"
+        >
+          <BsBoxSeam className="mr-2" /> Products
+        </Link>
+
+        <Link
+          className="flex flex-row items-center mb-4 text-white hover:text-blue-200"
+          to="/admin/orders"
+        >
+          <BsCart4 className="mr-2" /> Orders
+        </Link>
+
+        <Link
+          className="flex flex-row items-center text-white hover:text-blue-200"
+          to="/admin/customers"
+        >
+          <BsPeopleFill className="mr-2" /> Customers
+        </Link>
+      </div>
+
+      <div className="w-[80%] h-screen bg-red-600">
+        <Routes path="/*">
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+          <Route path="/orders" element={<h1>Orders</h1>} />
+          <Route path="/customers" element={<h1>Customers</h1>} />
+          <Route path="/*" element={<h1>404 not found the admin page</h1>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
