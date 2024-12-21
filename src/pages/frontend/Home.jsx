e5a0cdb2d61c0b2f9df000fb329a933bbae24de7
Deshan Chinthana <chamildes@gmail.com>
2024-12-21 18:21:06 +0530

    Home page design changes

--- src/pages/frontend/Home.jsx
+++ src/pages/frontend/Home.jsx
@@ -1,15 +1,18 @@
-import ProductCard from "../../components/frontend/home/ProductCard";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex justify-center p-5">
+      <div className="home-container text-center">
+        <h2 className="text-2xl text-blue-800 font-semibold my-8">Welcome to Our Website!</h2>
+        <p className="mb-10">
+          Explore a world of possibilities with our services. We are here to make your life easier and more enjoyable.
+        </p>
 
-      <div className="flex sm-flex-col justify-between">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <div>
+          <Link to="/login" className="bg-blue-700 hover:bg-blue-500 text-white p-4 mt-3">Login</Link>
+        </div>
       </div>
-
-    </>
+    </div>
   )
 }
