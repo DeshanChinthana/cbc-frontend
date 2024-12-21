1fd66600a97c06e0c84d78be9418871ec2033ba9
Deshan Chinthana <chamildes@gmail.com>
2024-12-21 17:47:27 +0530

    Tested code samples

--- src/components/frontend/home/ProductCard.jsx
+++ src/components/frontend/home/ProductCard.jsx
@@ -1,6 +1,6 @@
 export default function ProductCard() {
   return (
-    <div className="product-card">
+    <div className="product-card border-2 border-blue-900 bg-blue-400">
         <h3>Card Title</h3>
         <h5>Card Sub Title</h5>
         <p>Card description comes HERE</p>
--- src/pages/frontend/Home.jsx
+++ src/pages/frontend/Home.jsx
@@ -3,10 +3,12 @@ import ProductCard from "../../components/frontend/home/ProductCard";
 export default function Home() {
   return (
     <>
-        
+
+      <div className="flex sm-flex-col justify-between">
         <ProductCard />
         <ProductCard />
         <ProductCard />
+      </div>
 
     </>
   )
